58e0e6d453ce332a6ac2c2ca3c9d3f2de9be5252
PyAbhisek <abhisek.praharaj@styledotme.com>
2024-12-07 13:42:56 +0530

    added logout feature

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { AppContext } from "../Context/AppContextProvider";
 import { useNavigate } from 'react-router-dom';
 import FeedPosts from "./FeedPosts";
+import logoutIcon from "../Assests/logout.svg"
 const Feed = () => {
     const context = useContext(AppContext);
     const navigate = useNavigate()
@@ -9,16 +10,26 @@ const Feed = () => {
         throw new Error("AppContext must be used within a AppContextProvider");
     }
 
-    const { userInfo } = context;
+    const { userInfo, setUserInfo } = context;
+
+    const logout = () => {
+        setUserInfo({});
+        window.sessionStorage.clear();
+        navigate('/');
+    }
     return (
         <>
-            <div onClick={() => { navigate("/profile") }} className="flex items-center h-[3.125rem] ml-[1rem] mt-[1rem] ">
-                <img src={userInfo?.photoURL} alt="dp" className="w-[3.125rem] h-[3.125rem] rounded-full" />
-                <div className="ml-[0.625rem] flex flex-col   ">
-                    <p className="text-[0.625rem] leading-[0.77rem] font-kumbh text-[#00000054]">Welcome Back,</p>
-                    <p className="leading-[1.24rem] text-[1rem] font-kumbh font-semibold"> {userInfo?.Name || userInfo?.displayName}</p>
+            <div className="flex items-center justify-between h-[3.125rem] mx-[1rem] mt-[1rem] ">
+                <div className="flex  items-center " onClick={() => { navigate("/profile") }}>
+                    <img src={userInfo?.photoURL} alt="dp" className="w-[3.125rem] h-[3.125rem] rounded-full" />
+                    <div className="ml-[0.625rem] flex flex-col   ">
+                        <p className="text-[0.625rem] leading-[0.77rem] font-kumbh text-[#00000054]">Welcome Back,</p>
+                        <p className="leading-[1.24rem] text-[1rem] font-kumbh font-semibold"> {userInfo?.Name || userInfo?.displayName}</p>
 
+                    </div>
                 </div>
+
+                <img src={logoutIcon} alt="logoutIcon" onClick={logout} className="w-[1.125rem] h-[1.125rem]" />
 
             </div>
             <div className="ml-[1rem] mt-[1.9rem]">
